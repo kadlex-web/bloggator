7ab5c85909474bbea3a49744ec241d680fb4feb9
BenE <158221658+kadlex-web@users.noreply.github.com>
2026-02-23 08:37:58 -0500

    Update CreateFeedFollow SQL query for additional data
    Refactor SQL query to include user and feed names in the result set.

--- sql/queries/createfeedfollow.sql
+++ sql/queries/createfeedfollow.sql
@@ -1,5 +1,5 @@
 -- name: CreateFeedFollow :many
-WITH feeds AS (
+WITH insert_feed_follow AS (
     INSERT INTO feed_follows (id, created_at, updated_at, user_id, feed_id)
     VALUES (
         $1,
@@ -9,4 +9,10 @@ WITH feeds AS (
         $5
     ) RETURNING *
 )
-SELECT * FROM feeds;
+SELECT 
+    insert_feed_follow.*,
+    feeds.name AS feed_name
+    users.name AS user_name
+FROM feeds
+INNER JOIN users ON users.id = $4,
+INNER JOIN feeds ON feed.id = $5
